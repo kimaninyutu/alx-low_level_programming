62655627bc35ad38bb86af58e9e2b83f7813b923
Kimani Nyutu <capalot4802@gmail.com>
2022-09-14 13:04:54 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+0-putchar.c char alpha;
+
+0-putchar.c for (alpha = a; alpha <= z; alpha++)
+0-putchar.c {
+0-putchar.c 0-putchar.c _putchar(alpha);
+0-putchar.c }
+0-putchar.c _putchar(n);
+}
