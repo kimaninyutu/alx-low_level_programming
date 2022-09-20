fabb413ddba36d07bb796534eaf569bdd3a74e02
Kimani Nyutu <capalot4802@gmail.com>
2022-09-19 19:41:33 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: no return.
+ */
+void swap_int(int *a, int *b)
+{
+0-reset_to_98.c int c = *a;
+0-reset_to_98.c *a = *b;
+0-reset_to_98.c *b = c;
+}
